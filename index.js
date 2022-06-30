11426e1bb0558e712e7a9d24250b4f6addf83dc4
abrakib942 <abrakib942@gmail.com>
2022-07-01 02:57:22 +0600

    complete api

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ async function run() {
   try {
     await client.connect();
     const todoCollection = client.db("endgame").collection("todo-lists");
+    const completeCollection = client
+      .db("endgame")
+      .collection("completed-lists");
 
     app.post("/todo", async (req, res) => {
       const addTask = req.body;
@@ -41,6 +44,24 @@ async function run() {
         $set: task,
       };
       const result = await todoCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    app.post("/complete", async (req, res) => {
+      const completeTask = req.body;
+      const result = await completeCollection.insertOne(completeTask);
+      res.send(result);
+    });
+
+    app.get("/complete", async (req, res) => {
+      const result = await completeCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.delete("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await todoCollection.deleteOne(filter);
       res.send(result);
     });
   } finally {
